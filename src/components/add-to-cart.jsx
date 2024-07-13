2f42663c1f97d5a57dff13684d455e633c391582
srivishnu25 <thatisrivishnu@gmail.com>
2024-07-13 11:34:28 +0530

    major changes

---
+++ src/components/add-to-cart.jsx
@@ -0,0 +1,11 @@
+/** @jsxImportSource react */
+
+import { addItemToCart } from '../stores/cart';
+
+export const AddToCart = ({ item }) => {
+	return (
+		<button className="big-link" onClick={() => addItemToCart(item)}>
+			Add To Cart
+		</button>
+	);
+};
---
+++ src/stores/cart.js
@@ -0,0 +1,28 @@
+import { computed, map } from 'nanostores';
+
+export const $cart = map({});
+
+export function addItemToCart(item) {
+	const cartItem = $cart.get()[item.id];
+	const quantity = cartItem ? cartItem.quantity : 0;
+
+	$cart.setKey(item.id, {
+		item,
+		quantity: quantity + 1,
+	});
+}
+
+export function removeItemFromCart(itemId) {
+	$cart.setKey(itemId, undefined);
+}
+
+export const subtotal = computed($cart, (entries) => {
+	let subtotal = 0;
+	Object.values(entries).forEach((entry) => {
+		if (!entry) return;
+
+		subtotal += entry.quantity * entry.item.price;
+	});
+
+	return subtotal;
+});
